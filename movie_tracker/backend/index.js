d2c59f08c73df792dd82307d4c9d283bc94d0093
frunezshyna <fsdcayanan.student@ua.edu.ph>
2024-12-02 22:02:01 +0800

    Update index.js

--- movie_tracker/backend/index.js
+++ movie_tracker/backend/index.js
@@ -1,14 +1,24 @@
-// backend/index.js
+// On git bash
+// npm init -y
+// npm install express mysql2 dotenv
+
+// To Start
+// node index.js
+// or
+// npm start
+
+// Server Setup
 const express = require("express");
 const dotenv = require("dotenv");
-const movieRoutes = require("./route/Movie-Routes.js");  // Correct path to routes
+const movieRoutes = require("./route/Movie-Routes.js");
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
-app.use("/movies", movieRoutes);  // Set up the /movies route
+// Middlewares
+app.use(express.json());
+app.use("/movies", movieRoutes);
 
 const port = process.env.PORT || 3000;
 console.log('Port being used:', port);
